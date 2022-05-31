445e76ddfbfed60da57c3577910d7f2915c4e766
oriavel <58531404+oriavel@users.noreply.github.com>
2022-05-31 16:23:20 +0200

    final comments and fixes

--- src/main.js
+++ src/main.js
@@ -7,26 +7,22 @@ const boardMAX = 50;
 let xMAX, yMAX; // board dimensions
 let scents = []; // empty list of scents
 
-// manage line 0 of input
-function setDimensions(line) {
+function setDimensions(line) {      // manage line 0 of input
     let poss = line.split(" ");
     let x = parseInt(poss[0]);
     let y = parseInt(poss[1]);
     if (x >= boardMAX || y >= boardMAX || x < 0 || y < 0) {
         console.log("Wrong dimensions!");
-    } else {
-        // define limits for the board 
+    } else {         // define limits for the board 
         xMAX = x;
         yMAX = y;        
     }
 }
-
 function isBorder(position) {
     let x = parseInt(position.x); 
     let y = parseInt(position.y);
     return (x > (xMAX-1) || y > (yMAX - 1)|| x < 0 || y < 0);
 }
-  
 function fall(newPosition, state) { 
     let x = state.pos.x;
     let y = state.pos.y;
@@ -39,23 +35,21 @@ function fall(newPosition, state) {
         return {pos: newPosition, isFallen: state.isFallen};
     }
 }
-
 function processInstructions(acts, initPos){
     if (acts.length > 100) {
         console.log("Too many instructions!");
         return;
     }
-
     let out ="";                                    // output string
-    let state = {pos: initPos, isFallen:false};     // position and isFallen (assuming robot is initially placed inside the board)
+    let state = {pos: initPos, isFallen:false};     // position and isFallen
     for(let i = 0; i < acts.length; i++){
-        let p = state.pos;
+        let p = state.pos;  // previous state position
         if(!state.isFallen){
             state = fall(actions[acts[i]](p), state); // {{x, y, dir}, isFallen}      
             out = state.pos.x + " " + state.pos.y + " " + state.pos.dir;
         } else {
             out = p.x + " " + p.y + " " + p.dir + " LOST";
-            return out;
+            break;
         }
     }
     return out;
@@ -67,9 +61,8 @@ let output = "";
 setDimensions(lines[0]);
 let i = 1;
 while(i < lines.length){
-    // create robots in odd lines
     if(i%2 == 1){
-        // odd line = robot specification
+        // odd line = robot creation
         let line = lines[i];
         let coords = line.trim().split(" ");
         // ROBOT(x, y, dir, fallen) found unnecessary to make a class in this case
@@ -77,18 +70,9 @@ while(i < lines.length){
         let y = coords[1];
         let dir = coords[2];
         i++;
-
-        // even line = instructions
-        // process instructions in even lines, for robots created in previous iteration
-        output = output + processInstructions(lines[i].trim(), {x, y, dir}) + "\n";
-        // guardar output de robot
+        // even line = instructions for robots created in previous iteration
+        output = output + processInstructions(lines[i].trim(), {x, y, dir}) + "\n"; // save robots output
         i++;
     }
 }
-console.log(output);
-//console.log(scents);
-
-
-
-
-
+console.log(output); 
